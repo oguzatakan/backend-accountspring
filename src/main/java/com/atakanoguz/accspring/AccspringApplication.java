b22f03c1a6562fcb1c9bffe1d600e78f89fc2d49
Atakan <atakan17171717@hotmail.com>
2021-10-14 19:30:54 +0300

    project updates

--- src/main/java/com/atakanoguz/accspring/AccspringApplication.java
+++ src/main/java/com/atakanoguz/accspring/AccspringApplication.java
@@ -21,39 +21,38 @@ import java.util.function.Supplier;
 @SpringBootApplication
 public class AccspringApplication implements CommandLineRunner {
 
-	private final CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
-	public AccspringApplication(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
+    public AccspringApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccspringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccspringApplication.class, args);
+    }
 
-	@Bean
-	public OpenAPI customOpenAPI(@Value("${application-description}") String description,
-								 							@Value("${application-version}") String version){
-		return new OpenAPI()
-						.info(new Info()
-										.title("Account API")
-										.version(version)
-										.description(description)
-										.license(new License().name("Account API Licence")));
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+                                 @Value("${application-version}") String version) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Account API")
+                        .version(version)
+                        .description(description)
+                        .license(new License().name("Account API Licence")));
 
-	}
+    }
 
-	@Bean
-	public Clock clock() {
-		return Clock.systemUTC();
-	}
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
 
 
-
-	@Override
-	public void run(String... args) {
-		Customer customer = customerRepository.save(new Customer("","Atakan", "Oğuz",new HashSet<>()));
-		System.out.println(customer);
-	}
+    @Override
+    public void run(String... args) {
+        Customer customer = customerRepository.save(new Customer("", "Atakan", "Oğuz", new HashSet<>()));
+        System.out.println(customer);
+    }
 
 }
